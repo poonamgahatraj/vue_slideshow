36ecefb3829a502aaeb9162b63fa57731caba5c3
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-10-05 18:33:42 +0545

    slideshow

---
+++ slideshow/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import slideshow from './pages/slideshow.vue';
+</script>
+
+<template>
+ <slideshow/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ slideshow/src/pages/slideshow.vue
@@ -0,0 +1,65 @@
+<template>
+<div v-for="item in imageList" :key="item" class="slideshow-container">
+  <img :src="currentImg">
+
+  
+  
+
+  
+</div>
+<div>
+  <button @click="prev">Previous</button>
+  <button @click="next">Next</button>
+</div>
+
+</template>
+
+<script >
+export default{
+    data(){
+        return{
+          imageList:["public/assets/img_mountains_wide.jpg",
+          "public/assets/img1.jpg",
+          "public/assets/img2.jpg",
+          "public/assets/img3.jpg"
+        
+        ],
+        currentindex:0
+        
+        }
+    },
+
+    methods:{
+      currentImg(){
+        imageList[0]
+      },
+
+      prev(){
+        if (currentindex<=0)
+    {
+        
+        currentindex=imageList.length-1
+    }
+
+    if (currentindex>0)
+    {
+        currentindex=currentindex-1;
+    }
+  
+
+    imageList[currentindex];
+      },
+
+
+      
+
+      next(){
+
+      }
+      
+    }
+
+  }
+   
+
+</script>
