1acd69cb6bdd1bda2afa8637174b840a95a65808
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-10-06 15:57:59 +0545

    slideshow

--- slideshow/src/pages/slideshow.vue
+++ slideshow/src/pages/slideshow.vue
@@ -1,59 +1,78 @@
 <template>
-<div v-for="item in imageList" :key="item" class="slideshow-container">
-  <img :src="currentImg">
-
-  
-  
-
-  
-</div>
-<div>
+  <div class="container">
+    <img :src= "imageList[currentindex]">
+<div  class="btn">
   <button @click="prev">Previous</button>
   <button @click="next">Next</button>
 </div>
+  </div>
+ 
 
 </template>
 
 <script >
 export default{
     data(){
+
         return{
-          imageList:["public/assets/img_mountains_wide.jpg",
+          imageList:[
+            
+          "public/assets/img_mountains_wide.jpg",
           "public/assets/img1.jpg",
           "public/assets/img2.jpg",
           "public/assets/img3.jpg"
         
         ],
-        currentindex:0
+        currentindex:0,
+        current_location:""
         
         }
     },
 
     methods:{
-      currentImg(){
-        imageList[0]
-      },
-
+      
+     
       prev(){
-        if (currentindex<=0)
-    {
-        
-        currentindex=imageList.length-1
-    }
+         
+                  
 
-    if (currentindex>0)
-    {
-        currentindex=currentindex-1;
-    }
-  
+                  console.log("hellotest")
+                  if (this.currentindex<=0)
+              {
+                  
+                  this.currentindex=this.imageList.length-1
+                  
+              }
 
-    imageList[currentindex];
+              if (this.currentindex>0)
+              {
+                  this.currentindex=this.currentindex-1;
+              }
+            
+
+                current_location=this.imageList[currentindex];
       },
 
 
       
 
       next(){
+        console.log("next")
+       
+    if ( this.currentindex>this.imageList.length)
+    {
+        this.currentindex=this.imageList[0]
+        
+    }
+
+    if(this.currentindex<this.imageList.length)
+    {
+        this.currentindex++;
+    }
+
+   
+
+         current_location=this.imageList[currentindex];
 
       }
       
@@ -63,3 +82,17 @@ export default{
    
 
 </script>
+
+<style scoped>
+.container{
+  
+  display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+.btn{
+  display: flex;
+    justify-content: space-between;
+}
+</style>
